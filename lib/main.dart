3f3d07ef4b00dc0a659f23cbcf526c2628629388
JamesRodriguesFadami <james.rodrigues@fadami.com.br>
2021-04-24 00:05:25 -0300

    Criação da lista de contatos e do formulário de contato

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mybank2/screens/dashboard.dart';
 
 void main() {
   runApp(MyBank2App());
@@ -19,55 +20,6 @@ class MyBank2App extends StatelessWidget {
         ),
       ),
       home: Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DashBoard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Image.asset('images/dry-leaf-on-ground.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24.0,
-                  ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
