8572d354b10a7d38ffde83dae0267fff9f5600d4
JamesRodriguesFadami <james.rodrigues@fadami.com.br>
2021-04-25 20:58:24 -0300

    Configuração do builder do ListView para carregar  a lista de contatos armazenados no bd

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mybank2/screens/dashboard.dart';
 
-import 'database/app_database.dart';
-import 'models/contact.dart';
-
 void main() {
   runApp(MyBank2App());
-  save(Contact(0, 'João', 1000)).then((id) {
-    findAll().then((contacts) => debugPrint(contacts.toString()));
-  });
 }
 
 class MyBank2App extends StatelessWidget {
--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mybank2/database/app_database.dart';
+import 'package:mybank2/models/contact.dart';
 import 'package:mybank2/screens/contact_form.dart';
 
 class ContactsList extends StatelessWidget {
@@ -8,21 +10,38 @@ class ContactsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'James',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          ),
-        ],
+      body: FutureBuilder<List<Contact>>(
+        initialData: List(),
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch(snapshot.connectionState){
+
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [CircularProgressIndicator(), Text('Loading')],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return Text('Unknown Error');
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -37,6 +56,28 @@ class ContactsList extends StatelessWidget {
               );
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
