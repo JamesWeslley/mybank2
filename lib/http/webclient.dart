f0404d520aa96486e1497b78b9d9ca15815ef17f
JamesRodriguesFadami <james.rodrigues@fadami.com.br>
2021-05-16 19:42:37 -0300

    Instalação e aplicação de uso do plugin http_interceptor para interceptar os requests e responses do package http.

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,10 +1,33 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('Request');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+  );
   final url = Uri.https(
-    'f12814adeeca.ngrok.io',//https criado pelo ngrok
+    '58a830e10f5d.ngrok.io', //https criado pelo ngrok
     'transactions',
   );
-  final Response response = await get(url);
-  print(response.body);
+  final Response response = await client.get(url);
 }
